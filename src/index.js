8492df2767ab9081493336b72b78c27408ee85d1
Thomas Draxler <twdrax@gmail.com>
2018-07-21 18:45:14 +0700

    Reworking appearance. The goal of this app is to show current conditions in a city that the user looks up, so I'd like the appearance to change depending on the current conditions at a location. What you see with this update is code containing lots of removals (through commenting mainly) and importation of the Font Awesome module. Apart from that, the main css file has been added to.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee, faCloud } from '@fortawesome/free-solid-svg-icons'
+
 import './index.css';
+library.add(faCheckSquare, faCoffee, faCloud);
 
-const request = require('request');
 
-request('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});
+// const request = require('request');
 
-const loadScript = function(src) {
-    var tag = document.createElement('script');
-    tag.async = false;
-    tag.src = src;
-    document.getElementsByTagName('body').appendChild(tag);
-}
+// request('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1', { json: true }, (err, res, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body.url);
+//   console.log(body.explanation);
+// });
+
+// const loadScript = function(src) {
+//     var tag = document.createElement('script');
+//     tag.async = false;
+//     tag.src = src;
+//     document.getElementsByTagName('body').appendChild(tag);
+// }
 
 //Component for basic city info
 function CityProfile(props) {
     return (
         <div>
+            <FontAwesomeIcon icon="cloud" className="title"/>
             <h1>{props.name}, {props.state}</h1>
             <h2>Population: {props.population}</h2>
             <h3 className="text-format">{props.description}</h3>
@@ -61,9 +68,8 @@ class WeatherInterface extends React.Component {
     render() {
         return (
             <div className="page-appearance">
-                <h1>Find Weather Info Now!</h1>
-                <h4>Created by Thomas in 2018</h4>
-                <ActionButton />
+                <h1 className="title">instantWeather</h1>
+                <h4>Made by Thomas with React in 2018</h4>
                 <CityProfile
                     name={cityInfo.name}
                     state={cityInfo.state}
