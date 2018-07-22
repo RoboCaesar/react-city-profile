1613e512a95493f7528437765b429762941754e9
Thomas Draxler <twdrax@gmail.com>
2018-07-22 18:51:02 +0700

    Added an API call module in getcitydata.js. So far a city name is all that's displayed, but the API call is successful.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckSquare, faCoffee, faCloud } from '@fortawesome/free-solid-svg-icons'
 
 import './index.css';
+import {GetCityData} from './getcitydata.js';
+
 library.add(faCheckSquare, faCoffee, faCloud);
-
-
-// const request = require('request');
-
-// request('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1', { json: true }, (err, res, body) => {
-//   if (err) { return console.log(err); }
-//   console.log(body.url);
-//   console.log(body.explanation);
-// });
-
-// const loadScript = function(src) {
-//     var tag = document.createElement('script');
-//     tag.async = false;
-//     tag.src = src;
-//     document.getElementsByTagName('body').appendChild(tag);
-// }
 
 //Component for basic city info
 function CityProfile(props) {
@@ -46,30 +32,6 @@ function CityProfile(props) {
     );
 }
 
-//Search functionality component
-// class ActionButton extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {clicks: 0};
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     handleClick() {
-//         this.setState(prevState => ({
-//             clicks: prevState.clicks + 1
-//         }));
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h3>The button has been pressed {this.state.clicks} times.</h3>
-//                 <button type="button" onClick={this.handleClick}>Click Me</button>
-//             </div>
-//         );
-//     }
-// }
-
 //The main component
 class WeatherInterface extends React.Component {
     // constructor(props) {
@@ -89,6 +51,7 @@ class WeatherInterface extends React.Component {
                     population={cityInfo.population}
                     description={cityInfo.description}
                 />
+                <GetCityData />
             </div>
         );
     }
